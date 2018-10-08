6e8979c586341f96e45f173f16996055df75797e
pengyi <535719197@qq.com>
2018-10-08 16:00:48 +0800

    增加注释

--- AdalineGD.py
+++ AdalineGD.py
@@ -26,12 +26,13 @@ class AdalineGD(object):
     def fit(self, x, y):
         '''
         训练数据函数
-        :param x: {array-like}, shape=[n_samples, n_features]
+        :param x(DataDrame对象): {array-like}, shape=[n_samples, n_features]
                   训练向量(训练集)
                   n_samples: 样本数量
                   n_features: 特征数量
         :param y: array-like, shape=[n_smaples]
                   目标值(结果集)
+        :param w_: 权重向量
         :return:
         '''
         self.w_ = np.zeros(1 + x.shape[1])
@@ -39,16 +40,17 @@ class AdalineGD(object):
 
         for _ in range(self.n_iter):
             output = self.net_input(x)
-            errors = (y - output)
+            errors = (y - output)   # 真实样本和预测值的误差
             self.w_[1:] += self.eta * x.T.dot(errors)
             self.w_[0:] += self.eta * errors.sum()
-            cost = (errors ** 2).sum() / 2.0
+            cost = (errors ** 2).sum() / 2.0    # 损失函数
             self.cost_.append(cost)
         return self
 
     def net_input(self, x):
         '''
         计算净输入
+        对输入向量和权重向量做点积
         :param x:
         :return:
             两个矩阵的内积
@@ -58,6 +60,7 @@ class AdalineGD(object):
 
     def activation(self, x):
         '''
+        线性激活函数
         :param x:
         :return:
         '''
